7a7d1f8a02a617bbd4a184e23b2668183934a2b2
Rustam <rklepa_20@mail.ru>
2023-12-20 21:35:42 +0300

    1. исправление недочетов 2. новый тест ренейм

--- src/test/java/org/example/CsvManipulatorTest.java
+++ src/test/java/org/example/CsvManipulatorTest.java
@@ -7,6 +7,8 @@ public class CsvManipulatorTest {
     private  String testFileName;
     private  String newName;
     private  CsvManipulator csvManipulator;
+    private String testName;
+    private String newTestName;
 
     @BeforeClass
     public void setup() {
@@ -14,6 +16,8 @@ public class CsvManipulatorTest {
         csvManipulator = new CsvManipulator();
         testFileName = "test_file.csv";
         newName = "new_test_file.csv";
+        testName = "test_file_2.csv";
+        newTestName = "new_test_file_2.csv";
     }
     @Test (priority = 0)
     public void testCreateNewCsv() {
@@ -34,19 +38,29 @@ public class CsvManipulatorTest {
     @Test (priority = 3)
     public void testWrongName () {
         csvManipulator.editCsv("афафы","dsadas");
+        Assert.assertFalse(csvManipulator.searchCsvFile("афафы"));
     }
     @Test (priority = 4)
     public void testCreateNewCsv2 () {
         csvManipulator.createNewCsv(testFileName);
         csvManipulator.createNewCsv(testFileName);
+        Assert.assertTrue(csvManipulator.searchCsvFile(testFileName));
     }
     @Test (priority = 5)
     public void testDeleteCsv() {
         csvManipulator.deleteCsv(testFileName);
         Assert.assertFalse(csvManipulator.searchCsvFile(testFileName));
     }
+    @Test (priority = 6)
+    public void testRename() {
+        csvManipulator.createNewCsv(testName);
+        csvManipulator.updateCsv(testName,0,0,"что-то");
+        csvManipulator.editCsv(testName,newTestName);
+        Assert.assertEquals(csvManipulator.readField(newTestName, 0, 0), "что-то");
+    }
     @AfterClass
     public  void endClass() {
         csvManipulator.deleteCsv(newName);
+        csvManipulator.deleteCsv(newTestName);
     }
 }
